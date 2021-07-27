4be5c77e954f1a7a3304f1c1c2b2b452639c9469
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-07-26 22:24:24 -0500

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Index() {
+  return <Typography variant="h1">Next.js example</Typography>;
+}

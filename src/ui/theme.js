16d33823aa52708cf9f9dfa4aec520f3903eb488
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-07-27 11:54:44 -0500

    Set basic colors in theme

--- src/ui/theme.js
+++ src/ui/theme.js
@@ -1,20 +1,34 @@
-import { createTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createTheme } from "@material-ui/core/styles";
 
-// Create a theme instance.
+const diorioOrange = "#C16200";
+const diorioBlue = "#00546E";
+const diorioGrey = "#4D4D4D";
+
 const theme = createTheme({
   palette: {
+    common: {
+      orange: `${diorioOrange}`,
+      blue: `${diorioBlue}`,
+    },
     primary: {
-      main: '#556cd6',
+      main: `${diorioOrange}`,
     },
     secondary: {
-      main: '#19857b',
+      main: `${diorioBlue}`,
     },
-    error: {
-      main: red.A400,
+  },
+  typography: {
+    h1: {
+      color: diorioOrange,
     },
-    background: {
-      default: '#fff',
+    h2: {
+      color: diorioOrange,
+    },
+    subtitle: {
+      color: diorioGrey,
+    },
+    caption: {
+      color: diorioGrey,
     },
   },
 });

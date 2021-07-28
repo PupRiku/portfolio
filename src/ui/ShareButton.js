c256ecef64509b25ffa697238fe8f62c9fd21a0f
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-07-28 09:46:20 -0500

    Share post button beginning code

---
+++ src/ui/ShareButton.js
@@ -0,0 +1,35 @@
+import {
+    FacebookShareButton,
+    FacebookIcon,
+    TwitterShareButton,
+    TwitterIcon,
+    WhatsappShareButton,
+    WhatsappIcon,
+    LineShareButton,
+    LineIcon,
+  } from "react-share";
+  
+  const ShareButton = ({ url }) => (
+    <>
+      <FacebookShareButton url={url} style={{ outline: "none" }}>
+        <FacebookIcon size="32px" round />
+      </FacebookShareButton>
+      <TwitterShareButton
+        url={url}
+        style={{ marginLeft: `15px`, outline: "none" }}
+      >
+        <TwitterIcon size="32px" round />
+      </TwitterShareButton>
+      <WhatsappShareButton
+        url={url}
+        style={{ marginLeft: `15px`, outline: "none" }}
+      >
+        <WhatsappIcon size="32px" round />
+      </WhatsappShareButton>
+      <LineShareButton url={url} style={{ marginLeft: `15px`, outline: "none" }}>
+        <LineIcon size="32px" round />
+      </LineShareButton>
+    </>
+  );
+  
+  export default ShareButton;

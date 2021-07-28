2e1d38bf845b6f5f123a93bc6b33dde1021a287f
Chris Diorio <11452673+PupRiku@users.noreply.github.com>
2021-07-27 21:39:06 -0500

    Adjusted Index container

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,32 @@ import Link from "../src/Link";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Grid from "@material-ui/core/Grid";
 
+const useStyles = makeStyles(theme => ({
+  mainContainer: {
+    marginTop: "5em",
+    [theme.breakpoints.down("md")]: {
+      marginTop: "3em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginTop: "2em",
+    },
+  },
+}));
+
 export default function Index() {
+  const classes = useStyles();
+  const theme = useTheme();
+
   return (
-    <Grid container>
+    <Grid container direction="column" className={classes.mainContainer}>
       <Head>
         <title key="title">
           Chris Diorio - UX Designer and Front-End Developer
         </title>
       </Head>
-      <p>Home Page</p>
+      <Grid item>
+        <p>Home Page</p>
+      </Grid>
     </Grid>
   );
 }
